10d802c5f45597d296e7c5a12fd766096189809c
Vwant <wangvwei@outlook.com>
2018-03-18 13:35:26 +0800

    update git

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -7,7 +7,7 @@ namespace MyProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("Hello Home!");
         }
     }
 }
